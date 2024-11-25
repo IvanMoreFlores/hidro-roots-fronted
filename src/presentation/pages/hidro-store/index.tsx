6be93d41aa31d9920c0e1b43bc7c1e1c1e6046dd
Dilan-6 <ramosfloresangielodilan@gmail.com>
2024-11-24 19:36:07 -0500

    proyecto-actualizado

--- src/presentation/pages/hidro-store/index.tsx
+++ src/presentation/pages/hidro-store/index.tsx
@@ -4,7 +4,10 @@ import "./styles.css";
 import NavComponent from "../membership/components/navbar";
 import FooterComponent from "@/presentation/components/footer";
 import HidroStor from "./components/hidro-store/index";
-
+import MinistoreComponent from "./components/mini-store/index"
+import BuildComponent from "./components/we-build/index"
+import ContComponent  from "../about/components/contac"
+import UneComponent from "../about/components/unete"
 
 const HdirostoreComponent = () => {
     return (
@@ -12,6 +15,10 @@ const HdirostoreComponent = () => {
             <NavComponent />
             <main className="content">
                 <HidroStor />
+                <BuildComponent/>
+                <MinistoreComponent/>
+                <ContComponent/>
+                <UneComponent/>
             </main>
             <FooterComponent />
         </div>
