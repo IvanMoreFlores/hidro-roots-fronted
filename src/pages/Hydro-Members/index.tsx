e8228981d0b343a5b1ae1b62dbf8895a8958b63c
Ivan Joel More Flores <ivanyoe79@gmail.com>
2024-11-25 23:47:07 -0500

    Update

--- src/pages/Hydro-Members/index.tsx
+++ src/pages/Hydro-Members/index.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import "./styles.css";
 import "../../app/globals.css";
-import Nav1Component from "./components/navbar";
 import FooterComponent from "@/components/footer";
 import ContComponent from "././../about/components/contac";
 import UneComponent from "././../about/components/unete";
@@ -11,11 +10,12 @@ import StormenComponent from "./components/store-men/index";
 import ServiComponent from "./components/servic/index";
 import SpacebComponent from "./components/build-m/index";
 import CustComponent from "./components/customers/index";
+import NavbarComponent from "@/components/navbar";
 
 const HydromenPage = () => {
   return (
     <div className="landing-page">
-      <Nav1Component />
+      <NavbarComponent backgroundColor="rgba(188, 235, 241, 0.6)" />
       <main className="content">
         <HComponent />
         <StormenComponent />
--- src/pages/membership/index.tsx
+++ src/pages/membership/index.tsx
@@ -2,18 +2,18 @@
 import React from "react";
 import "./styles.css";
 import "../../app/globals.css";
-import NavComponent from "./components/navbar";
 import FooterComponent from "@/components/footer";
 import MenComponent from "./components/membres";
 import ImpComponent from "./components/inputs";
 import ContComponent from "././../about/components/contac";
 import UneComponent from "././../about/components/unete";
 import StoreComponent from "./components/store"
+import NavbarComponent from "@/components/navbar";
 
 const MembershiPage = () => {
   return (
     <div className="landing-page">
-      <NavComponent />
+      <NavbarComponent backgroundColor="rgba(255, 255, 255, 0.4)" />
       <main className="content">
         <MenComponent/>
         <ImpComponent/>
