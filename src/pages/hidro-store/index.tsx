107cbea7304ae045bf40bc64b6c002e13ecb852b
Ivan Joel More Flores <ivanyoe79@gmail.com>
2024-11-25 23:56:20 -0500

    Update file

--- src/pages/hidro-store/index.tsx
+++ src/pages/hidro-store/index.tsx
@@ -2,28 +2,28 @@
 import React from "react";
 import "./styles.css";
 import "../../app/globals.css";
-import NavComponent from "../membership/components/navbar";
 import FooterComponent from "@/components/footer";
 import HidroStor from "./components/hidro-store/index";
-import MinistoreComponent from "./components/mini-store/index"
-import BuildComponent from "./components/we-build/index"
-import ContComponent  from "../about/components/contac"
-import UneComponent from "../about/components/unete"
+import MinistoreComponent from "./components/mini-store/index";
+import BuildComponent from "./components/we-build/index";
+import ContComponent from "../about/components/contac";
+import UneComponent from "../about/components/unete";
+import NavbarComponent from "@/components/navbar";
 
 const HdirostoreComponent = () => {
-    return (
-        <div className="landing-page">
-            <NavComponent />
-            <main className="content">
-                <HidroStor />
-                <BuildComponent/>
-                <MinistoreComponent/>
-                <ContComponent/>
-                <UneComponent/>
-            </main>
-            <FooterComponent />
-        </div>
-    );
+  return (
+    <div className="landing-page">
+      <NavbarComponent backgroundColor="rgba(255, 255, 255, 0.4)" />
+      <main className="content">
+        <HidroStor />
+        <BuildComponent />
+        <MinistoreComponent />
+        <ContComponent />
+        <UneComponent />
+      </main>
+      <FooterComponent />
+    </div>
+  );
 };
 
 export default HdirostoreComponent;
